bf59a61571a4699ed213d200b74d65b28ef51fdb
Niccolò Salvi <nicco.salvi@gmail.com>
2024-04-05 11:48:25 +0200

    :art: JSON Deserializer for loading objectives card mantaining polymorphism

---
+++ src/main/java/it/polimi/ingsw/gc38/model/ObjectiveCardDeserializer.java
@@ -0,0 +1,19 @@
+package it.polimi.ingsw.gc38.model;
+
+import com.google.gson.*;
+
+import java.lang.reflect.Type;
+
+class ObjectiveCardDeserializer implements JsonDeserializer<Objective> {
+    @Override
+    public Objective deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+        JsonObject jsonObject = json.getAsJsonObject();
+        String pattern = jsonObject.get("pattern").getAsString();
+
+        return switch (pattern) {
+            case "position" -> context.deserialize(jsonObject, PositionObjective.class);
+            case "resource" -> context.deserialize(jsonObject, ResourceObjective.class);
+            default -> throw new JsonParseException("Unknown pattern: " + pattern);
+        };
+    }
+}
