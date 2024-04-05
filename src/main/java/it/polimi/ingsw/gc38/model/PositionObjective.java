6cdec0e9237143c130246a35efb49baee081e2bb
Niccolò Salvi <nicco.salvi@gmail.com>
2024-04-05 11:51:21 +0200

    :bricks: Improving and testing new structure

--- src/main/java/it/polimi/ingsw/gc38/model/PositionObjective.java
+++ src/main/java/it/polimi/ingsw/gc38/model/PositionObjective.java
@@ -3,7 +3,7 @@ package it.polimi.ingsw.gc38.model;
 //import java.util.List;
 //import java.util.Map;
 
-public abstract class PositionObjective extends Objective{
+public abstract class PositionObjective extends Objective {
 
     public Card getCard(Player p, int id) {
             Card cardSearched = p.getCodex().stream().filter(card -> card.getId() == id).findFirst().orElse(null);
