e88ffdbbdd94707e3832891bb74c07fb5bf67e40
Niccolò Salvi <nicco.salvi@gmail.com>
2024-04-08 15:07:28 +0200

    :sparkles: Implemented visualization of DownL Objective

--- src/main/java/it/polimi/ingsw/gc38/model/DownLObjective.java
+++ src/main/java/it/polimi/ingsw/gc38/model/DownLObjective.java
@@ -2,9 +2,10 @@ package it.polimi.ingsw.gc38.model;
 
 import it.polimi.ingsw.gc38.view.CliView;
 
-//import java.util.List;
-//import java.util.Map;
+import java.util.List;
+
 public class DownLObjective extends PositionObjective{
+    private List<Color> colors;
     private Color color1;
     private Color color2;
 
@@ -13,6 +14,32 @@ public class DownLObjective extends PositionObjective{
     }
     public void setColor2(Color color) {
         this.color2 = color;
+    }
+
+    public void displayCard(CliView view) {
+        final String ANSI_RED_BACKGROUND = "\u001B[41m";
+        final String ANSI_COLOR_RESET = "\u001B[0m";
+        final String ANSI_BOLD = "\u001B[1m";
+        final String ANSI_COLOR_GOLD = "\u001B[33m";
+        final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+        final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
+        final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+
+        String ANSIColor = "";
+
+        if (getColor() == Color.RED) {
+            ANSIColor = ANSI_RED_BACKGROUND;
+        } else if (getColor() == Color.BLUE) {
+            ANSIColor = ANSI_BLUE_BACKGROUND;
+        } else if (getColor() == Color.PURPLE) {
+            ANSIColor = ANSI_PURPLE_BACKGROUND;
+        } else if (getColor() == Color.GREEN) {
+            ANSIColor = ANSI_GREEN_BACKGROUND;
+        }
+
+        view.displayMessage(ANSIColor + "  " + ANSI_BOLD + ANSI_COLOR_GOLD + this.getPoints() + ANSI_COLOR_RESET + ANSIColor + "  " + colors.get(0).toString().charAt(0) + " " + ANSI_COLOR_RESET);
+        view.displayMessage(ANSIColor + "    " + colors.get(1).toString().charAt(0) + "  " + ANSI_COLOR_RESET);
+        view.displayMessage(ANSIColor + "    " + colors.get(2).toString().charAt(0) + "  " + ANSI_COLOR_RESET);
     }
 
     public void CalculatePoints(Player p) {
