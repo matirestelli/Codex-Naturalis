d7d847b40447fa213a074f96c83948fb4659e12d
Niccolò Salvi <nicco.salvi@gmail.com>
2024-04-15 11:31:42 +0200

    🧹 Removed unused code

--- src/main/java/it/polimi/ingsw/gc38/model/Deck.java
+++ src/main/java/it/polimi/ingsw/gc38/model/Deck.java
@@ -1,44 +1,22 @@
 package it.polimi.ingsw.gc38.model;
 
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Map;
 import java.util.List;
 
 public class Deck {
     private List<CardGame> cards;
-    private String configFile;
+    private final String configFile;
     private boolean empty;
 
     public Deck(String configFile) {
         this.configFile = "src/main/resources/it/polimi/ingsw/gc38/" + configFile + ".json";
         this.cards = new ArrayList<>();
-    }
-
-    public void setCards(List<CardGame> cards) {
-        this.cards = cards;
-    }
-
-    public List<CardGame> getCards() {
-        return cards;
-    }
-
-    public void setConfigFile(String configFile) {
-        this.configFile = configFile;
-    }
-
-    public String getConfigFile() {
-        return configFile;
-    }
-
-    public void setEmpty(boolean empty) {
-        this.empty = empty;
     }
 
     public boolean isEmpty() {
@@ -47,10 +25,6 @@ public class Deck {
 
     public void shuffle() {
         Collections.shuffle(this.cards);
-    }
-
-    public void removeCard(Card card) {
-        this.cards.remove(card);
     }
 
     public void loadCardsFromJSON(Type typeOfCard, Gson gson) {
