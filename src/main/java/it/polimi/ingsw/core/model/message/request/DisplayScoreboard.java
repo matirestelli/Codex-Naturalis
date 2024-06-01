5358784f2e1a9d8d7a27af4af57fcef43cd7785b
Marcoscianna <marcosc2001@gmail.com>
2024-06-01 12:46:18 +0200

    scoreboard, notify numbers of message

--- src/main/java/it/polimi/ingsw/core/model/message/request/DisplayScoreboard.java
+++ src/main/java/it/polimi/ingsw/core/model/message/request/DisplayScoreboard.java
@@ -1,2 +1,18 @@
-package it.polimi.ingsw.core.model.message.request;public class DisplayScoreboard {
+package it.polimi.ingsw.core.model.message.request;
+
+import it.polimi.ingsw.core.model.Objective;
+import it.polimi.ingsw.network.ClientAbstract;
+
+import java.util.List;
+import java.util.Map;
+
+public class DisplayScoreboard extends MessageServer2Client{
+    public DisplayScoreboard(String type, Object data) {
+        super(type, data);
+    }
+
+    @Override
+    public void doAction(ClientAbstract client) {
+        client.getUIStrategy().displayScoreboard((Map<String,Integer>) getData());
+    }
 }
